cf18e4efbf7451080cc00e52a6ec58067577db44
Jeff Byrnes <jeff@evertrue.com>
2015-02-17 12:56:49 -0500

    Run tests as Ubuntu 14.04 using ChefSpec::SoloRunner

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -19,9 +19,9 @@
 require 'spec_helper'
 
 describe 'mcrouter::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'when all attributes are default, on Ubuntu 14.04,' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new platform: 'ubuntu', version: '14.04'
       runner.converge(described_recipe)
     end
 
