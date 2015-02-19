5a389a4e73c207a803472c4ba5c7349dd15f2aca
Jeff Byrnes <jeff@evertrue.com>
2015-02-19 18:22:02 -0500

    Consistent parantheses for tests
    Because.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'mcrouter::default' do
   context 'installs mcrouter to the $PATH' do
-    describe command('which mcrouter') do
+    describe command 'which mcrouter' do
       its(:stdout) { is_expected.to match(%r{/usr/local/bin/mcrouter}) }
     end
   end
