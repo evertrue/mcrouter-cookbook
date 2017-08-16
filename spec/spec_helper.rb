566109e7f7533ae66b952d8880b12ced5644ced4
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-08-16 15:28:41 -0400

    Update ChefSpec coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,11 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'coveralls'
 
+# Report coverage to Coveralls
 Coveralls.wear!
+
+# Generate a report
+ChefSpec::Coverage.start!
 
 RSpec.configure do |config|
   config.formatter = :documentation
@@ -10,5 +14,3 @@ RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
 end
-
-at_exit { ChefSpec::Coverage.report! }
