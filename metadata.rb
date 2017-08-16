4d7976e4ac69576a6d2b61957e71794df2640ff7
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-08-16 15:23:59 -0400

    Add Supermarket URLs to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'apache2'
 description      'Installs/Configures mcrouter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.2'
+source_url       'https://github.com/evertrue/mcrouter-cookbook'
+issues_url       'https://github.com/evertrue/mcrouter-cookbook/issues'
 
 supports 'ubuntu', '= 14.04'
 
