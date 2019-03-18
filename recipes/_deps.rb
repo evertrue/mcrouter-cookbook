193fe3c3820a8d198cc6ada282e36931bcc8152d
Jeff Byrnes <thejeffbyrnes@gmail.com>
2019-03-18 14:05:47 -0400

    Use build_essential resource
    Provides forward-compat with Chef 14 & greater

--- recipes/_deps.rb
+++ recipes/_deps.rb
@@ -16,7 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'build-essential'
+apt_update
+build_essential 'mcrouter'
 
 %w(
   automake
