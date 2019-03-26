f5a6c12fe82cb649df71c00919b4d9df713657e2
Jeff Byrnes <thejeffbyrnes@gmail.com>
2019-03-26 10:53:30 -0400

    Refactor to use Ubuntu 16.04 & 18.04 APT package
    * Simplify installation
    * Drop building & installing from source in favor of APT package
    * Add support for Ubuntu 16.04, 18.04
    * Drop support for Ubuntu 14.04 b/c it is EOL
    * Refactor to use kitchen-dokken for faster test runs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,5 @@
-default['mcrouter']['src_dir'] = '/opt/mcrouter'
-default['mcrouter']['install_dir'] = "#{node['mcrouter']['src_dir']}/install"
+default['mcrouter']['bin_path'] = '/usr/bin/mcrouter'
 default['mcrouter']['user'] = 'mcrouter'
-default['mcrouter']['version'] = 'v0.5.0'
-
-default['folly']['src_dir'] = '/opt/folly'
-default['folly']['version'] = 'v0.53.0'
 
 default['mcrouter']['local_memcached'] = true
 default['memcached']['port'] = 11_811
@@ -15,7 +10,6 @@ default['mcrouter']['cli_opts'] = {
   'config-file' => '/etc/mcrouter/mcrouter.json',
   'async-dir'   => '/var/spool/mcrouter',
   'log-path'    => '/var/log/mcrouter/mcrouter.log',
-  'pid-file'    => '/var/run/mcrouter/mcrouter.pid',
   'stats-root'  => '/var/mcrouter/stats',
 }
 
